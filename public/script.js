a4a67249d1108d9c0e9b78c47f527bb40d68509b
sakura <vaggelis817@gmail.com>
2023-09-09 18:38:01 +0300

    update

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = io();
+//const socket = io();
 
 const displayController = (() => {
   const renderMessage = (message) => {
@@ -52,25 +52,76 @@ const Game = (() => {
   let players = [];
   let currentPlayerIndex;
   let gameOver;
+  let isPlayer1Turn;
+  let roundStarted = false;
 
 
-  socket.on('update board', (data) => {
-    Gameboard.update(data.index,data.mark);
-  })
+  /* socket.on('update board', (data) => {
+     Gameboard.update(data.index, data.mark);
+     if (currentPlayerIndex === 0) {
+       document.querySelector('#Player1').classList.add('active-player');
+       document.querySelector('#Player2').classList.remove('active-player');
+     } else {
+       document.querySelector('#Player1').classList.remove('active-player');
+       document.querySelector('#Player2').classList.add('active-player');
+     }
+     isPlayer1Turn = !isPlayer1Turn;
+     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+   }); 
+   socket.on('name-updated', (data) => {
+     console.log(data);
+     document.querySelector('#Player1').value = data.player1;
+     document.querySelector('#Player2').value = data.player2;
+     if (data.player1 !== undefined) {
+       document.querySelector('#Player1').value = data.player1;
+     }
+     if (data.player2 !== undefined) {
+       document.querySelector('#Player2').value = data.player2;
+     }
+   });
+    */
 
 
   const start = () => {
+    const playerName1 = document.querySelector('#Player1').value.trim();
+    const playerName2 = document.querySelector('#Player2').value.trim();
+    const errorMessageElement = document.getElementById('errorMessage');
+    roundStarted = true;
+    if (!playerName1 || !playerName2) {
+      errorMessageElement.textContent = 'Both player names must be filled out to start the game!';
+
+      // Add the error effect class
+      errorMessageElement.classList.add('errorShow');
+
+      // Remove the effect after some time (e.g., 1 second)
+      setTimeout(() => {
+        errorMessageElement.classList.remove('errorShow');
+      }, 1000);
+
+      return; // IMPORTANT! This prevents the rest of the function from running when there's an error
+    } else {
+      errorMessageElement.textContent = ''; // Clear the error message
+    }
     players = [
       createPlayer(document.querySelector('#Player1').value, 'X'),
       createPlayer(document.querySelector('#Player2').value, 'O')
     ]
     currentPlayerIndex = 0;
     gameOver = false;
+    isPlayer1Turn = true;
     Gameboard.render();
-
+    roundStarted = 'true';
   }
 
   const handleClick = (event) => {
+    if (currentPlayerIndex === 0) {
+      document.querySelector('#Player1').classList.add('active-player');
+      document.querySelector('#Player2').classList.remove('active-player');
+    } else {
+      document.querySelector('#Player2').classList.add('active-player');
+      document.querySelector('#Player1').classList.remove('active-player');
+    }
+
     if (gameOver) {
       return;
     }
@@ -79,32 +130,50 @@ const Game = (() => {
     if (Gameboard.getGameboard()[index] === '') {
       const currentPlayer = players[currentPlayerIndex];
       const markColor = currentPlayer.mark === 'X' ? 'red' : 'blue';
-      Gameboard.update(index, players[currentPlayerIndex].mark, markColor);
 
-      socket.emit('move made', { index: index, mark: players[currentPlayerIndex].mark });
-      if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)) {
-        gameOver = true;
-        displayController.renderMessage(`${players[currentPlayerIndex].name} wins`)
+      if ((isPlayer1Turn && currentPlayer.mark === 'X') || (!isPlayer1Turn && currentPlayer.mark === 'O')) {
+        Gameboard.update(index, players[currentPlayerIndex].mark, markColor);
 
-      } else if (checkForTie(Gameboard.getGameboard())) {
-        gameOver = true;
-        displayController.renderMessage('Its a tie');
+        /*socket.emit('move made', {
+          index: index,
+          mark: players[currentPlayerIndex].mark,
+          player1: players[0].name,
+          player2: players[1].name
+        });*/
+        if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)) {
+          gameOver = true;
+
+          const winnerMessageElement = document.getElementById('message');
+          winnerMessageElement.classList.add('errorShow');
+          displayController.renderMessage(`${players[currentPlayerIndex].name} wins`);
+          displayController.renderMessage(`${players[currentPlayerIndex].name} wins`)
+
+        } else if (checkForTie(Gameboard.getGameboard())) {
+          gameOver = true;
+          displayController.renderMessage('Its a tie');
+        }
+        isPlayer1Turn = !isPlayer1Turn;
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
       }
-      currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     }
-  }
+  };
+
 
   const restart = () => {
+    if (!roundStarted) {
+      return;
+    }
+
     for (let i = 0; i < 9; i++) {
       Gameboard.update(i, '');
     }
-    currentPlayerIndex = 0;
+
 
     document.querySelector('#message').innerHTML = '';
-
     gameOver = false;
+    currentPlayerIndex = 0;
+    Game.start();
     Gameboard.render();
-
   }
 
   return {
@@ -147,4 +216,7 @@ restartButton.addEventListener('click', () => {
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
   Game.start();
-})
+  let playerName1 = document.querySelector('#Player1').value;
+  let playerName2 = document.querySelector('#Player2').value;
+  /*socket.emit('set-name', { player1: playerName1, player2: playerName2 });*/
+});
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express');
+/*
+ const express = require('express');
 const path = require('path');
 const http = require('http');
 
@@ -16,10 +17,20 @@ io.on('connection', (socket) => {
   console.log('A user connected with socket ID:', socket.id);
 
   socket.on('move made', (data) => {
+    socket.broadcast.emit('name-updated', {player1: data.player1, player2: data.player2});
     console.log(`Received 'move made' from socket ID: ${socket.id}. Data:`, data);
     // Broadcast the move to the other player
     socket.broadcast.emit('update board', data);
   });
+  socket.on('set-name', (data) => {
+    console.log(data);
+    // Assuming `io` is your socket.io instance
+    io.sockets.emit('name-updated', {
+        player1: data.player1,
+        player2: data.player2
+    });
+});
+
 
   socket.on('disconnect', () => {
     console.log(`User with socket ID: ${socket.id} disconnected.`);
@@ -28,3 +39,4 @@ io.on('connection', (socket) => {
 server.listen(3000,'0.0.0.0', () => {
   console.log('Listening on port 3000');
 });
+  
